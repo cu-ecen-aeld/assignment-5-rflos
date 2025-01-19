070b70469e2c2018241746c20f0d92b14eba4216
Ruei-Fa Tseng <74195702+rflos@users.noreply.github.com>
2025-01-19 21:14:48 +0800

    code change for assignment7

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = 'f6ce9f5'
+AESD_ASSIGNMENTS_VERSION = '72e4d77'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
